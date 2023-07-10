2d3a452baee876124d5c4d51c59bb3db4ebc218e
Sam Bulatov <mephistorine@gmail.com>
2023-07-10 09:57:27 +0300

    Nvim config init

---
+++ plugins/user.lua
@@ -0,0 +1,30 @@
+return {
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+  "projekt0n/github-nvim-theme",
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_by_name = {
+            ".DS_Store",
+            ".git"
+          }
+        }
+      },
+      window = {
+        position = "right"
+      }
+    }
+  }
+}
