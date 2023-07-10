7196822fc0b19aad3896b44966bbbcb14dd54721
Sam Bulatov <mephistorine@gmail.com>
2023-07-10 10:01:35 +0300

    Change default theme

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "github_dark_dimmed",
+  colorscheme = "github_light",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
